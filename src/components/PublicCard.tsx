f0eb08cb24a05a0bb6c4affb425efd1cb1682024
new8oro <new8pro@basexindustries.com>
2025-11-16 12:10:03 +0300

    Updated PublicCard.tsx

--- src/components/PublicCard.tsx
+++ src/components/PublicCard.tsx
@@ -42,6 +42,15 @@ export default function PublicCard({ slug }: PublicCardProps) {
     }
   }, [card]);
 
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const shareParam = params.get('share');
+
+    if (shareParam === 'contact' && card?.allow_contact_sharing) {
+      setShowContactShare(true);
+    }
+  }, [card]);
+
   const loadCard = async () => {
     try {
       const data = await getBusinessCardBySlug(slug);
